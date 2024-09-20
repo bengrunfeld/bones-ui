5de95890bef592d1a2b5c2d060cc74dd8b3c4819
bengrunfeld <benjamin.grunfeld@gmail.com>
2024-09-20 12:26:28 +0300

    Update project configuration

--- src/CustomSelect/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as CustomSelect, SelectOption } from "./CustomSelect";
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as Button } from "./Button/Button";
+export { default as CheckBox } from "./CheckBox/CheckBox";
+export { default as Input } from "./Input/Input";
+export { default as RadioButtonGroup } from "./RadioButtonGroup/RadioButtonGroup";
+export { default as Select } from "./Select/Select";
